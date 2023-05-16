2a534986926c9b16c235704f4d4f7f54a42ced42
Andrey <andrbrd@gmail.com>
2023-05-16 22:10:02 +0300

    add sidebar background img

--- src/features/sidebar-content/index.tsx
+++ src/features/sidebar-content/index.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import { Wrap } from "../../components/wrap";
 import { Stepper } from "../../components/stepper";
 import { SidebarContentProps } from "./types";
-
+import SidebarBgImg from "../../assets/bg.png";
 export const SidebarContent: React.FC<SidebarContentProps> = React.memo(
   (props) => {
     const { currentStep, steps } = props;
     return (
-      <Wrap sx={{ height: "100%", margin: "4.2rem 2.6rem" }}>
+      <Wrap
+        sx={{
+          height: "100%",
+          padding: "4.2rem 2.6rem",
+          flexDirection: "column",
+          backgroundImage: `url(${SidebarBgImg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: "bottom"
+        }}
+      >
         <Stepper currentStep={currentStep} steps={steps} vertical />
       </Wrap>
     );
